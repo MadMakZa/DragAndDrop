d46be71cd4427f45efe95cbf6a3e483a77a68d0a
Maximus <immakza@gmail.com>
2022-05-14 01:19:40 +0300

    Composable fun DragTarget

--- app/src/main/java/com/makza/draganddrop/DragAndDrop.kt
+++ app/src/main/java/com/makza/draganddrop/DragAndDrop.kt
@@ -1,5 +1,6 @@
 package com.makza.draganddrop
 
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,6 +8,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntSize
 
@@ -65,5 +68,29 @@ fun <T> DragTarget(
 ) {
     var currentPosition by remember { mutableStateOf(Offset.Zero)}
     val currentState = LocalDragTargetInfo.current
-    
+
+    Box(modifier = modifier
+        .onGloballyPositioned {
+            currentPosition = it.localToWindow(Offset.Zero)
+        }
+        .pointerInput(Unit){
+            detectDragGesturesAfterLongPress(onDragStart = {
+                currentState.dataToDrop = dataToDrop
+                currentState.isDragging = true
+                currentState.dragPosition = currentPosition + it
+                currentState.draggableComposable = content
+            }, onDrag = { change, dragAmount ->
+                change.consumeAllChanges()
+                currentState.dragOffset += Offset(dragAmount.x, dragAmount.y)
+            }, onDragEnd = {
+                currentState.isDragging = false
+                currentState.dragOffset = Offset.Zero
+            }, onDragCancel = {
+                currentState.dragOffset = Offset.Zero
+                currentState.isDragging = false
+            })
+        }
+    ){
+        content()
+    }
 }
