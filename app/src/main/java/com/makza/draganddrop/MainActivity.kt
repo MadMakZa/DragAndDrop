34cffbc3b454d128bbf81513ff64fd1445e51036
Maximus <immakza@gmail.com>
2022-05-14 01:51:48 +0300

    Composable add fun FoodItemCard()

--- app/src/main/java/com/makza/draganddrop/MainActivity.kt
+++ app/src/main/java/com/makza/draganddrop/MainActivity.kt
@@ -3,13 +3,22 @@ package com.makza.draganddrop
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.makza.draganddrop.ui.theme.DragAndDropTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +26,75 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DragAndDropTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                        text = "Drag and Drop Ui Element",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            )
+                        }
+                    ) {
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DragAndDropTheme {
-        Greeting("Android")
+fun FoodItemCard(
+    foodItem: FoodItem
+){
+    Card(
+        elevation = 10.dp,
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(24.dp),
+        modifier = Modifier.padding(8.dp)
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(10.dp)
+        ) {
+            DragTarget(
+                modifier = Modifier.size(130.dp),
+                dataToDrop = foodItem
+            ) {
+                Image(
+                    painter = painterResource(id = foodItem.image),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(130.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Column(modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = foodItem.name,
+                        fontSize = 22.sp,
+                        color = Color.DarkGray
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+                    Text(
+                        text = "$${foodItem.price}",
+                        fontSize = 18.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                }
+            }
+        }
     }
 }
